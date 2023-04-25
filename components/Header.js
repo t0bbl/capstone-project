183deaafd8f89303a64fee5ed408fbe8ce2dc8d4
robin droste <117731660+t0bbl@users.noreply.github.com>
2023-04-25 16:03:28 +0200

    init header.js

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Head = styled.p`
+  display: flex;
+  flex-direction: center;
+`;
+
+export default function Header() {
+  return (
+    <div>
+      <Head>AiMySh!rtUp</Head>
+    </div>
+  );
+}
