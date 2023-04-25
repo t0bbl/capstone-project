9bb402e912335826f28803dca23c1a8899453b67
robin droste <117731660+t0bbl@users.noreply.github.com>
2023-04-25 16:53:50 +0200

    form styling, not done

--- components/Form.js
+++ components/Form.js
@@ -3,31 +3,41 @@ import styled from "styled-components";
 const FormContainer = styled.form`
   display: grid;
   gap: 0.5rem;
+  background-color: yellow;
+  color: teal;
 `;
 
 const Input = styled.input`
   padding: 0.5rem;
   border: 3px solid black;
   border-radius: 0.5rem;
-`;
-
-const Textarea = styled.textarea`
-  border: 3px solid black;
-  border-radius: 0.5rem;
-  padding: 0.5rem;
+  background-color: yellow;
+  color: teal;
+  width: 90%;
+  text-align: center;
 `;
 
 export default function Form() {
   return (
     <FormContainer>
-      <Input id="firstKeyword" name="firstKeyword" type="text" value="FIRST" />
+      <Input
+        id="firstKeyword"
+        name="firstKeyword"
+        type="text"
+        placeholder="FIRST"
+      />
       <Input
         id="secondKeyword"
         name="secondKeyword"
         type="text"
-        value="SECOND"
+        placeholder="SECOND"
       />
-      <Input id="thirdKeyword" name="thirdKeyword" type="text" value="THIRD" />
+      <Input
+        id="thirdKeyword"
+        name="thirdKeyword"
+        type="text"
+        placeholder="THIRD"
+      />
     </FormContainer>
   );
 }
