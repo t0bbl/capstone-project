0fbe20f2b96ee0f79284df4d6e9594b7ee475c4e
robin droste <117731660+t0bbl@users.noreply.github.com>
2023-04-26 16:17:28 +0200

    added four placeholder pics and aligned them

---
+++ components/ChooseFourPics.js
@@ -0,0 +1,32 @@
+import Image from "next/image.js";
+
+export default function ChooseFourPics() {
+  return (
+    <>
+      <Image
+        alt="image number 1"
+        src="https://i.imgur.com/SglfvOg.jpeg/"
+        width="100"
+        height="100"
+      />
+      <Image
+        alt="image number 1"
+        src="https://i.imgur.com/SglfvOg.jpeg/"
+        width="100"
+        height="100"
+      />
+      <Image
+        alt="image number 1"
+        src="https://i.imgur.com/SglfvOg.jpeg/"
+        width="100"
+        height="100"
+      />
+      <Image
+        alt="image number 1"
+        src="https://i.imgur.com/SglfvOg.jpeg/"
+        width="100"
+        height="100"
+      />
+    </>
+  );
+}
