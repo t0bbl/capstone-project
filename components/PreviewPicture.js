e93ac52cf49deda6975f2effee7cb7dc8b9ac946
robin droste <117731660+t0bbl@users.noreply.github.com>
2023-04-27 10:10:05 +0200

    data structure for previewimage and styledimage

---
+++ components/PreviewPicture.js
@@ -0,0 +1,7 @@
+export default function PreviewPicture() {
+  return (
+    <>
+      <StyledImage></StyledImage>
+    </>
+  );
+}
---
+++ pages/PreviewPage/index.js
@@ -0,0 +1,10 @@
+import Header from "@/components/Header";
+
+export default function PreviewPicture() {
+  return (
+    <>
+      <Header />
+      <PreviewPicture />
+    </>
+  );
+}
