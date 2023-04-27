a2f5eb5b13fd00bd69dac6aa0ffcd5630dab2f48
robin droste <117731660+t0bbl@users.noreply.github.com>
2023-04-27 11:37:15 +0200

    work on links in PreviewPage

--- pages/PreviewPage/index.js
+++ pages/PreviewPage/index.js
@@ -1,17 +1,37 @@
 import { StyledButton } from "@/components/StyledButton";
 import styled from "styled-components";
-
+import { useRouter } from "next/router";
 import Header from "../../components/Header";
 import PreviewPicture from "../../components/PreviewPicture";
 
 export default function PreviewPage() {
+  const router = useRouter();
+
+  function handleClick(event) {
+    event.preventDefault();
+
+    router.push(event.target.value);
+  }
   return (
     <>
       <Header />
       <Container>
         <PreviewPicture />
-        <StyledButton type="click" className="variations">
+        <StyledButton
+          type="button"
+          value="/ChooseFour/"
+          className="variations"
+          onClick={handleClick}
+        >
           Give me Variations!
+        </StyledButton>
+      </Container>
+      <Container className="bottomButtons">
+        <StyledButton type="button" value="/Saved/" onClick={handleClick}>
+          SAVE
+        </StyledButton>
+        <StyledButton type="button" value="/Print/" onClick={handleClick}>
+          PRINT
         </StyledButton>
       </Container>
     </>
@@ -25,4 +45,10 @@ const Container = styled.div`
   align-items: center;
   margin-top: 10vw;
   gap: 20vw;
+  &.bottomButtons {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-items: center;
+  }
 `;
