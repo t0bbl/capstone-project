82740559c8260a0947f04dc58d4c19cf6b5c60c1
robin droste <117731660+t0bbl@users.noreply.github.com>
2023-05-09 09:44:47 +0200

    blabla

--- pages/api/openai/index.js
+++ pages/api/openai/index.js
@@ -2,8 +2,8 @@ import { Configuration, OpenAIApi } from "openai";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    const { keywordOne, keywordTwo, keywordThree } = req.body;
-    await fetchImages(res, { keywordOne, keywordTwo, keywordThree });
+    const { searchID, keywordOne, keywordTwo, keywordThree } = req.body;
+    await fetchImages(res, { searchID, keywordOne, keywordTwo, keywordThree });
   } else {
     res.status(405).send("Method not allowed");
   }
@@ -13,25 +13,34 @@ async function fetchImages(
   res,
   { keywordOne, keywordTwo, keywordThree, searchID }
 ) {
-  const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
-  const openai = new OpenAIApi(configuration);
-  const response = await openai.createImage({
-    prompt:
-      "give me a design with a " +
-      keywordOne +
-      " and a " +
-      keywordTwo +
-      " and a " +
-      keywordThree,
-    n: 2,
-    size: "1024x1024",
-  });
-  const imageData = response.data;
-  res.status(200).json({ image: imageData.data });
+  try {
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+    const response = await openai.createImage({
+      prompt:
+        "give me a design with a " +
+        keywordOne +
+        " and a " +
+        keywordTwo +
+        " and a " +
+        keywordThree,
+      n: 4,
+      size: "1024x1024",
+    });
+    const imageData = response.data;
+    res
+      .status(200)
+      .json({ image: imageData.data, formattedData: formattedData });
 
-  const formattedData = imageData.data.map((item, index) => {
-    return { [`pic${index + 1}`]: { picSRC: item.url } };
-  });
+    const formattedData = imageData.data.map((item, index) => {
+      return { [`pic${index + 1}`]: { picSRC: item.url } };
+    });
+  } catch (error) {
+    console.error("Error while fetching images:", error);
+    res
+      .status(500)
+      .json({ message: "An error occurred while processing your request." });
+  }
 }
