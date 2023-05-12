a9f0f78d4687f014b6ca0a334e5c319709f4399f
robin droste <117731660+t0bbl@users.noreply.github.com>
2023-05-12 16:18:21 +0200

    small fix

--- components/Header.js
+++ components/Header.js
@@ -5,10 +5,10 @@ const Headline = styled.h1`
   width: 120%;
   height: 120%;
   display: flex;
+  font-weight: 800;
   align-items: center;
   justify-content: center;
-  font-weight: 300;
-  font-size: 1.5rem;
+  font-size: 3rem;
   font-size: 5vh;
   margin: 0;
   padding: 1rem 0rem;
--- components/StyledText.js
+++ components/StyledText.js
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 
 export const StyledText = styled.p`
+  font-weight: 600;
   width: 110%;
   height: 110%;
   color: cyan;
-  font-size: 2.5vh;
+  font-size: 3vh;
   text-align: center;
   margin: 15vw 3vw;
   /* margin: 20% 5%; */
