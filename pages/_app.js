a7121402b39698bb5ce8c70812ddebe81d9eec7b
robin droste <117731660+t0bbl@users.noreply.github.com>
2023-05-15 16:51:26 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,7 @@ import GlobalStyle from "../styles";
 import Background from "@/components/Background";
 import { useEffect } from "react";
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    const pix = document.getElementsByClassName("pixel");
 
-    for (var i = 0; i < pix.length; i++) {
-      pix[i].style.animationDelay = Math.ceil(Math.random() * 5000) + "ms";
-    }
-  });
   return (
     <>
       <Background />
