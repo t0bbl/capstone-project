b42aa691d214b69dbb258814e191ed1a1c34def5
robin droste <117731660+t0bbl@users.noreply.github.com>
2023-05-17 11:23:06 +0200

    Update Container.js

--- components/Container.js
+++ components/Container.js
@@ -7,8 +7,8 @@ export const Container = styled.div`
   flex-direction: column;
   justify-content: space-evenly;
   align-items: center;
-  margin-top: 10vw;
-  gap: 20vw;
+  margin-top: 10vh;
+  gap: 8vh;
 
   ${(props) =>
     props.bottomButtons &&
