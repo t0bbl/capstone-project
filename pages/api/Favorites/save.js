0af5d577fc1ac4f767d23966a06e1c728ec3083b
robin droste <117731660+t0bbl@users.noreply.github.com>
2023-05-23 21:39:58 +0200

    error handling update

--- pages/api/Favorites/save.js
+++ pages/api/Favorites/save.js
@@ -10,9 +10,13 @@ export default async function handler(req, res) {
       console.log("favoriteData:", favoriteData);
       const favorite = new Favorite(favoriteData);
       await favorite.save();
-      res.status(201).json({ status: "shirt created" });
+      res.status(201).json({ status: "fav created" });
     } catch (error) {
       res.status(400).json(console.error(error));
     }
+  } else {
+    // If it's not a POST method, return a 405 Method Not Allowed
+    console.log(`Method ${req.method} not allowed`);
+    res.status(405).json({ status: "Error", error: "Method not allowed" });
   }
 }
