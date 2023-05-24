f032cc7be06f2b58c5e67a55903c61379a6fbb2e
robin droste <117731660+t0bbl@users.noreply.github.com>
2023-05-24 16:50:44 +0200

    another small fix

--- pages/AlltimeFavorites/index.js
+++ pages/AlltimeFavorites/index.js
@@ -18,7 +18,7 @@ export default function AlltimeFavorites() {
     data: fetchedFavorites,
     isLoading,
     error,
-  } = useSWR(`/api/favorites/all`, fetcher);
+  } = useSWR(`/api/favorites`, fetcher);
 
   if (isLoading || !fetchedFavorites) {
     return <Loading />;
